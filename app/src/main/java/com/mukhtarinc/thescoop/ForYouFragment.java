8a9d225b4f3fc8a5bfadeb177c5774bce229b0f1
raiyan-aegon <mukhtarraiyan16994@gmail.com>
2020-05-23 19:06:11 +0000

    Successfully Added Bottom Navigation and Fragments

---
+++ app/src/main/java/com/mukhtarinc/thescoop/ForYouFragment.java
@@ -0,0 +1,69 @@
+package com.mukhtarinc.thescoop;
+
+import android.os.Bundle;
+
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mukhtarinc.thescoop.databinding.FragmentForYouBinding;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ForYouFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public ForYouFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ShelfFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ForYouFragment newInstance(String param1, String param2) {
+        ForYouFragment fragment = new ForYouFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        FragmentForYouBinding binding = DataBindingUtil.inflate(inflater,R.layout.fragment_for_you,container,false);
+
+
+        return binding.getRoot();
+    }
+}
--- app/src/main/java/com/mukhtarinc/thescoop/MainActivity.java
+++ app/src/main/java/com/mukhtarinc/thescoop/MainActivity.java
@@ -1,10 +1,17 @@
 package com.mukhtarinc.thescoop;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.mukhtarinc.thescoop.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,5 +25,49 @@ public class MainActivity extends AppCompatActivity {
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
 
 
+        ForYouFragment forYouFragment = ForYouFragment.newInstance("","");
+        openFragment(forYouFragment);
+
+
+        binding.bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+
+                    case R.id.foryou:
+                        Toast.makeText(MainActivity.this, "For you", Toast.LENGTH_SHORT).show();
+                        ForYouFragment forYouFragment = ForYouFragment.newInstance("","");
+                        openFragment(forYouFragment);
+                        break;
+                    case R.id.today:
+                        Toast.makeText(MainActivity.this, "Today", Toast.LENGTH_SHORT).show();
+                        TodayFragment todayFragment = TodayFragment.newInstance("","");
+                        openFragment(todayFragment);
+                        break;
+                    case R.id.follow:
+                        Toast.makeText(MainActivity.this, "Following", Toast.LENGTH_SHORT).show();
+                        FollowingFragment followingFragment = FollowingFragment.newInstance("","");
+                        openFragment(followingFragment);
+                        break;
+                    case R.id.shelf:
+                        Toast.makeText(MainActivity.this, "Shelf", Toast.LENGTH_SHORT).show();
+                        ShelfFragment shelfFragment = ShelfFragment.newInstance("","");
+                        openFragment(shelfFragment);
+                        break;
+
+                }
+                return true;
+            }
+        });
+
+
+    }
+
+    void openFragment(Fragment fragment){
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container,fragment)
+                .addToBackStack(null)
+                .commit();
     }
 }
