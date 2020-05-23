567f106e0da5657e84d794e9642ff302b8ce5357
raiyan-aegon <mukhtarraiyan16994@gmail.com>
2020-05-23 02:37:58 +0000

    Adding all dependencies to project

--- app/src/main/java/com/mukhtarinc/thescoop/MainActivity.java
+++ app/src/main/java/com/mukhtarinc/thescoop/MainActivity.java
@@ -1,14 +1,22 @@
 package com.mukhtarinc.thescoop;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import com.mukhtarinc.thescoop.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+
     }
 }
